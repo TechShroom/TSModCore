6ad03b445598c5c4cfcc2851725f7bd26cd2aa6c
Kenzie Togami <ket1999@gmail.com>
2015-03-12 11:20:54 -0700

    Cleanup Javadocs

--- src/com/techshroom/mods/common/java8/package-info.java
+++ src/com/techshroom/mods/common/java8/package-info.java
@@ -1,8 +1,9 @@
 /**
- * <p>
  * This package contains many Java 8-like utilities that make for much cleaner code.
  * 
- * <p>These classes are usually modeled after Guava look-alikes.
+ * <p>
+ * These classes are usually modeled after Guava look-alikes.
+ * </p>
  * 
  * @author Kenzie Togami
  */
